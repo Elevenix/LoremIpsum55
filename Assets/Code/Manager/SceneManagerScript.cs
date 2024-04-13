861e881fbc463287130dfc478a65022592507c96
Mickael-95 <72389929+Mickael-95@users.noreply.github.com>
2024-04-13 18:52:56 +0200

    scene manager script updated

--- Assets/Code/Manager/SceneManagerScript.cs
+++ Assets/Code/Manager/SceneManagerScript.cs
@@ -27,7 +27,7 @@ public class SceneManagerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(this);
+
     }
 
     // Update is called once per frame
