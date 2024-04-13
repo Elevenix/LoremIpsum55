2cc104506d218be794ea2161af2bdc501a4eb0dd
Mickael-95 <72389929+Mickael-95@users.noreply.github.com>
2024-04-13 13:10:04 +0200

    Player Movement, Jump and Gravity

---
+++ Assets/Code/Scripts/PlayerMovement.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rb;
+    [SerializeField]
+    private Transform groundCheck;
+    [SerializeField]
+    private LayerMask groundLayer;
+    private float horizontal;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float jumpingPower;
+    private bool isPlayerFacingRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        if (!isPlayerFacingRight && horizontal > 0f)
+        {
+            Flip();
+        } else if (isPlayerFacingRight && horizontal < 0f)
+        {
+            Flip();
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        isPlayerFacingRight = !isPlayerFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(context.performed && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if(context.canceled && rb.velocity.y > 0.0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+}
